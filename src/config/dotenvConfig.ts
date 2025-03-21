9d37e56eec4398c1b573fdce3286a83696e5614e
Ofir <ofir@som.co.il>
2025-03-22 01:55:59 +0200

    wip

--- src/config/dotenvConfig.ts
+++ src/config/dotenvConfig.ts
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 5002;
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://ofir:ofir@ofirdb.nj9y0.mongodb.net/?retryWrites=true&w=majority&appName=Driving';
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://ofir:ofir@cluster0.24z57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
